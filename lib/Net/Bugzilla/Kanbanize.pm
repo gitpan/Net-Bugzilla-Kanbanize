50596f31c76dfc158211ec27e88bf4186e370581
Philippe M. Chiasson <gozer@cpan.org>
2014-06-19 18:48:38 +0000

    Import of GOZER/Net-Bugzilla-Kanbanize-0.001-TRIAL from CPAN.
    gitpan-cpan-distribution: Net-Bugzilla-Kanbanize
    gitpan-cpan-version:      0.001-TRIAL
    gitpan-cpan-path:         GOZER/Net-Bugzilla-Kanbanize-0.001-TRIAL.tar.gz
    gitpan-cpan-author:       GOZER
    gitpan-cpan-maturity:     developer

---
+++ lib/Net/Bugzilla/Kanbanize.pm
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+
+package Net::Bugzilla::Kanbanize;
+$Net::Bugzilla::Kanbanize::VERSION = '0.001'; # TRIAL
+#ABSTRACT: turns baubles into trinkets
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Net::Bugzilla::Kanbanize - turns baubles into trinkets
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SYNOPSIS
+
+Kanbanize Bugzilla Sync Tool
+
+=head1 METHODS
+
+=head2 new
+
+This method does something experimental.
+
+=head2 bar
+
+This method returns a reason.
+
+1;
+
+=head1 AUTHOR
+
+Philippe M. Chiasson <gozer@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by Philippe M. Chiasson.
+
+This is free software, licensed under:
+
+  Mozilla Public License Version 2.0
+
+=cut
